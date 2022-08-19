8def97fba584c37d5b4d5214a468c7dd6acd6b3a
halimindra <halimindrakusuma@gmail.com>
2022-08-19 10:58:48 +0700

    mengganti routes dan login page

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'auth';
+$route['default_controller'] = 'welcome';
 /* $route['admin/dashboard'] = 'admin/dashboard';
 $route['admin/barang'] = 'admin/barang'; */
 $route['admin/(:any)'] = 'admin/$1';
